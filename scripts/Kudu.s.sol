8e323bf0fd2edf57dee2cd7370b489a3fbb90c96
xFNighTMaRe <brodriguezserrano54@gmail.com>
2023-06-09 02:35:19 +0200

    add first version of kudu token

---
+++ scripts/Kudu.s.sol
@@ -0,0 +1,48 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import "@openzeppelin/contracts/proxy/transparent/TransparentUpgradeableProxy.sol";
+import "forge-std/Script.sol";
+import "../src/Kudu.sol";
+
+/**
+ * @dev From the root folder, execute the following command to deploy and verify the contract.
+ * forge script scripts/Kudu.s.sol:Deploy --rpc-url <your_rpc_url> --etherscan-api-key <your_etherscan_api_key> --verify --broadcast -vvv
+ */
+contract Deploy is Script {
+  function run() external {
+    address deployerPublicKey = vm.envAddress("PUBLIC_KEY");
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+    vm.startBroadcast(deployerPrivateKey);
+
+    Kudu logic = new Kudu();
+    TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(
+      address(logic),
+      deployerPublicKey,
+      abi.encodeWithSignature("initialize()")
+    );
+    vm.stopBroadcast();
+    console.log("Implementation address: ", address(logic));
+    console.log("Proxy address: ", address(proxy));
+  }
+}
+
+/**
+ * @dev From the root folder, execute the following command to upgrade and verify the contract.
+ * forge script scripts/Kudu.s.sol:Deploy --rpc-url <your_rpc_url> --etherscan-api-key <your_etherscan_api_key> --verify --broadcast -vvv
+ */
+contract Upgrade is Script {
+  function run() external {
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+    address kuduProxyAddress = vm.envAddress("KUDU_PROXY_ADDRESS");
+    vm.startBroadcast(deployerPrivateKey);
+
+    Kudu logic = new Kudu();
+    ITransparentUpgradeableProxy(kuduProxyAddress).upgradeTo(address(logic));
+
+    vm.stopBroadcast();
+    console.log("New implementation address: ", address(logic));
+  }
+}
--- src/Kudu.sol
+++ src/Kudu.sol
@@ -1,12 +1,12 @@
-//SPDX-License-Identifier: MIT License
+//SPDX-License-Identifier: GPL-3.0-or-later
 
 pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 
 contract Kudu is ERC20Upgradeable {
-    function initialize() external initializer {
-        __ERC20_init("Kudu", "KUDU");
-        _mint(msg.sender, 10e6 * 10e18);
-    }
+  function initialize() external initializer {
+    __ERC20_init("Kudu", "KUDU");
+    _mint(msg.sender, 10e6 * 10e18);
+  }
 }
